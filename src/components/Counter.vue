814423218fee5f62a672ab441eec22dbc37180a2
Berkan Cetinkaya <berkan.cetinkaya@windowslive.com>
2018-07-12 19:04:20 +0300

    feat: add Count getCount getter

---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+   Counter: {{getCount}}
+
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex';
+export default {
+  name: 'Counter',
+  computed: mapGetters(['getCount'])
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const state = {
+  count: 0
+}
+
+const getters = {
+  getCount: state => state.count
+}
+
+export default new Vuex.Store({
+  state,
+  getters
+})
